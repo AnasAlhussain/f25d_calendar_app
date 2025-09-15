8316ac27117229c92faab6319d9bd55d518ce9b9
Anas ALhussain <anasalhussain.sweeden@hotmail.com>
2025-09-15 16:07:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CalandarApp from "./Components/CalandarApp"
+import './Components/CalandarApp.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalandarApp/>
+    </div>
+  )
+}
+
+export default App
+
